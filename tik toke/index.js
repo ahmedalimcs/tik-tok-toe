d3f3a8ff8ddbc0f95538573820f2a0a884a7b699
ahmedalimcs <86968780+ahmedalimcs@users.noreply.github.com>
2021-09-05 17:47:15 +0500

    Add files via upload

---
+++ tik toke/index.js
@@ -0,0 +1,83 @@
+// var a = document.getElementById("c1")
+
+// var b = document.getElementById("c2")
+
+// var c = document.getElementById("c3")
+
+// var d = document.getElementById("c4")
+
+// var e = document.getElementById("c5")
+
+// var f = document.getElementById("c6")
+
+// var g = document.getElementById("c7")
+
+// var h = document.getElementById("c8")
+
+// var i = document.getElementById("c9")
+
+// // empty value store in array
+
+// var x = ["","","","","","","","","",]
+
+// var b = PlAyerX;
+
+// var c = Player0;
+  
+
+  
+
+  
+  
+
+
+
+// function startgame(){
+//     if((userselect==X ) && (userselect==X) && (userselect==X){
+
+//     }
+(
+var b = document.getElementById("div")
+
+var c = document.getElementById("good")
+
+function radiocheck(){
+    for(i=0; radio<value.length; i++){
+
+    
+     if(radio[i]=="check")
+   alert("plz check your radio")
+
+   }
+
+//    validation email
+//  ahmedalimcs21@gmail.com
+  var f = document.getElementById("div")
+
+  function emailvalidation(){
+      if(emailvalidation.com.indexof()==-1){
+        alert("email address correct")
+      } else{
+          ("email address not correct")
+      }
+  }
+
+//   exeption try and catch 
+     
+     
+      function greeting(){
+          var a = "assalam o alikum";
+          aler("greeting")
+      }
+         try(""){
+
+         }
+
+         catch ("error"){
+           aler()
+         }
+         
+
+
+
+
